894adb4c6ee04db3aff0c4801536afe93154f97c
Destiny Samuel <destinysamuel37@gmail.com>
2019-10-02 18:02:49 +0000

    Automatically backed up by Learn

---
+++ case.rb
@@ -0,0 +1,11 @@
+greeting= "unfriendly_greeting"
+
+case greeting
+when
+  "unfriendly_greeting"
+  puts "What do you what!?"
+  when 
+    "friendly_greeting"
+    puts "Hi! How are you?"
+    
+  end
---
+++ method.rb
@@ -0,0 +1,3 @@
+def get_stock_market_data(date)
+  return nil if is_a_weekend?(date)
+end
